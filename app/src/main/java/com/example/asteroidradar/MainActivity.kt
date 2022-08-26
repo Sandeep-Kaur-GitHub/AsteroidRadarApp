5caa74073a637859f44af550c3e2566dbb31a727
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-08-26 22:57:17 +0530

    Fetch data from NeoWS API using retrofit library and display raw Json String on the textView

--- app/src/main/java/com/example/asteroidradar/MainActivity.kt
+++ app/src/main/java/com/example/asteroidradar/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.asteroidradar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.asteroidradar.network.NeoAPI
-import retrofit2.Call
-import retrofit2.Callback
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.example.asteroidradar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var  viewModel:MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel=ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.lifecycleOwner=this
+        binding.viewModel = viewModel
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
- 
+
 }
---
+++ app/src/main/java/com/example/asteroidradar/MainViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.asteroidradar
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.asteroidradar.network.NeoAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+    private val _response = MutableLiveData<String>()
+    val response: LiveData<String>
+        get() = _response
+
+    init {
+        getNeoWSProperties()
+    }
+
+    private fun getNeoWSProperties() {
+        NeoAPI.retrofitService.getProperties().enqueue(object : Callback<String> {
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+            }
+
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+
+        })
+    }
+}
